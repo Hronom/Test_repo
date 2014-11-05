c0fa827fe4875839b3857a310b2a5435383008ae
Eugene <hronom@gmail.com>
2014-11-05 14:57:46 +0200

    Test commit to branch 1.0.0

--- src/main/java/Test/App.java
+++ src/main/java/Test/App.java
@@ -4,10 +4,11 @@ package Test;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hi!" );
     }
 }
