e15f6ee3dc99007037078dd8f87156da1dd0626c
Hronom <hronom@gmail.com>
2014-11-06 14:08:15 +0200

    Fix 2.1.0 message

--- src/main/java/Test/App.java
+++ src/main/java/Test/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         // Test
-        System.out.println( "<Hello World>" );
+        System.out.println( "Hello World!" );
         System.out.println( 2 + 2 );
         System.out.println( 2 * 2 );
     }
